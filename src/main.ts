48616acf80af1458a277b64b86f02f813e95facd
sdqguallichicogt <sdq.guallichico@gmail.com>
2025-04-29 10:19:10 -0500

    crud-funcional

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,19 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('MainProduct');
+
+  console.log(envs.natsServers)
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
-      transport: Transport.TCP,
+      transport: Transport.NATS,
       options: {
-        port: envs.port
+        servers: envs.natsServers
       }
     }
   );
+
+  
   
   app.useGlobalPipes(
     new ValidationPipe({
